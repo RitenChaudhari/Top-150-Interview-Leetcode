87cf9074aa74e3a7333f555872c048a340ab0e03
RitenChaudhari <ritenchaudhari@gmail.com>
2026-02-03 23:26:36 +0530

    Daily Leetcode Challenge

---
+++ Miscellaneous/3637-Trionic-Array-1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        int n = nums.size();
+        int i =0;
+
+        while(i+1<n && nums[i]<nums[i+1]){
+            i++;
+        }
+
+        if(i == 0 || i == n-1) return false;
+
+        while(i+1<n && nums[i] > nums[i+1]){
+            i++;
+        }
+
+        if(i==n-1) return false;
+
+        while(i+1<n && nums[i] < nums[i+1]){
+            i++;
+        }
+
+       return i==n-1;
+    }
+};
