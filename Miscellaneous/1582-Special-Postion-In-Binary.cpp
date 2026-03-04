09f1c129a76521fd3be4ce9fdb2558aedec12bec
RitenChaudhari <ritenchaudhari@gmail.com>
2026-03-04 20:44:18 +0530

    Binary Positions

---
+++ Miscellaneous/1582-Special-Postion-In-Binary.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+
+        int count = 0;
+        for(int row=0;row<m;row++){
+            for(int col=0;col<n;col++){
+
+                if(mat[row][col]== 0) continue;
+                
+                bool special = true;
+                for(int r=0;r<m;r++){
+                    if(r != row && mat[r][col] == 1){
+                        special = false;
+                        break;
+                    }
+                }
+
+                for(int c=0;c<n;c++){
+                    if(c!= col && mat[row][c] == 1){
+                        special = false;
+                        break;
+                    }
+                }
+
+                if(special == true) count++;
+            }
+        }
+
+        return count;
+    }
+};
