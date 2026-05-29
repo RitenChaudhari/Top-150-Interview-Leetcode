1b65837e2bdb7da7071c05915a817d4aec370631
RitenChaudhari <ritenchaudhari@gmail.com>
2026-05-29 21:37:17 +0530

    DailyQ3310

---
+++ Miscellaneous/3300-Min-Element-After-Replacement.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minElement(vector<int>& nums) {
+        int n = nums.size();
+
+        int minVal = INT_MAX;
+        int numSum = 0;
+
+        for(int i=0;i<n;i++){
+            while(nums[i] > 0){
+                int lastDig = nums[i]%10;
+                numSum += lastDig;
+                nums[i] /= 10;
+            }
+
+            minVal = min(minVal,numSum);
+            numSum = 0;
+        }
+
+        return minVal;
+    }
+};
